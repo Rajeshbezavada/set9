45afe87b978db1884f482801d1d278620e807e42
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-09-05 00:13:48 -0400

    Create  Kabali.java

---
+++ Kabali.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ class Kabali
+ {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=b-a;
+		System.out.println(c);
+	}
+
+}
